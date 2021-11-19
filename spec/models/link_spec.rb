a3ceb22fda877cf12ad3c671813c72ef3b830c53
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-19 17:52:32 -0500

    adds spec to check for key presence

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
   it { is_expected.to validate_url_of(:external_url) }
+  it 'creates short key after saving link' do
+    link = build(:link)
+    link.save
+    expect(link.short_key).not_to be_truthy
+  end
 end
