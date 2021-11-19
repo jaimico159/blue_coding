4ae864b21d76615e02f06915de6a7e4857c7e288
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2021-11-19 17:26:20 -0500

    copied encode implementation to get short key

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,26 @@
 class Link < ApplicationRecord
+  ALPHABET =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split(//)
+
+  after :generate_short_key
+
+  private
+
+  def generate_short_key
+    short_key = bijective_encode(id)
+  end
+
+  def bijective_encode(i)
+    # from http://refactormycode.com/codes/125-base-62-encoding
+    # with only minor modification
+    return ALPHABET[0] if i == 0
+
+    s = ''
+    base = ALPHABET.length
+    while i > 0
+      s << ALPHABET[i.modulo(base)]
+      i /= base
+    end
+    s.reverse
+  end
 end
